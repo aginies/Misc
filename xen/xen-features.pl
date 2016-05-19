03abd83870182b23b2679fe7438023d2e4b4f0bb
aginies <aginies@suse.com>
2016-05-19 11:48:22 +0200

    display XEN version, display XEN capabilities, show Disabled feature(s) available in kernel

--- xen/xen-features.pl
+++ xen/xen-features.pl
@@ -5,9 +5,18 @@
 # This will get out of date. If you're a Xen developer, you are welcome to put
 # this under xen/tools/misc, where others can update it.
 #
+# 16-May-2016	Antoine Ginies 
+#	display XEN version
+#	display XEN capabilities
+#	show Disabled feature(s) available in kernel
 # 05-May-2014	Brendan Gregg	Created this.
 
 use strict;
+
+sub cat_ {
+  open(my $F, $_[0]) or return; my @l = <$F>; wantarray() ? @l : join '', @l;
+}
+
 
 open FEAT, "/sys/hypervisor/properties/features" or die "ERROR open(): $!";
 my $features = <FEAT>;
@@ -15,13 +24,34 @@ close FEAT;
 chomp $features;
 my $decfeatures = hex $features;
 
+my $VERSIONDIR= "/sys/hypervisor/version/";
+my @files = ("major", "minor", "extra");
+my $version;
+foreach my $f (@files) {
+	foreach (cat_("$VERSIONDIR/$f")) {
+	chomp ($_);
+	# add missing . between major and minor
+	if ($f =~ /major/) { $_ = $_ . "." }
+	$version = $version . $_;
+}}
+
+my $capa = cat_("/sys/hypervisor/properties/capabilities");
+
+print "Xen version: $version \n";
+print "Xen capabilities: $capa \n";
 print "Xen features: $features\n";
 
 foreach (<DATA>) {
 	my ($def, $feat, $bit) = split;
 	$feat =~ s/^XENFEAT_//;
-	print "enabled: $feat\n" if $decfeatures & (1 << $bit);
+	if ($decfeatures & (1 << $bit)) {
+		print "+ ENABLED: $feat\n" 
+	} else {
+		print "-Disable: $feat\n" 
+	}
 }
+
+print "\nhttp://xenbits.xen.org/docs/unstable-staging/hypercall/x86_64/include,public,features.h.html\n";
 
 # The following are from include/xen/interface/features.h, and will need updating:
 

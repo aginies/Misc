a390e88a30fe8a68cc96a94b3d89bfa1d78351d0
aginies <aginies@suse.com>
2016-05-19 11:49:58 +0200

    perltidy the script (better code layout)

--- xen/xen-features.pl
+++ xen/xen-features.pl
@@ -5,7 +5,7 @@
 # This will get out of date. If you're a Xen developer, you are welcome to put
 # this under xen/tools/misc, where others can update it.
 #
-# 16-May-2016	Antoine Ginies 
+# 16-May-2016	Antoine Ginies
 #	display XEN version
 #	display XEN capabilities
 #	show Disabled feature(s) available in kernel
@@ -14,9 +14,10 @@
 use strict;
 
 sub cat_ {
-  open(my $F, $_[0]) or return; my @l = <$F>; wantarray() ? @l : join '', @l;
+    open( my $F, $_[0] ) or return;
+    my @l = <$F>;
+    wantarray() ? @l : join '', @l;
 }
-
 
 open FEAT, "/sys/hypervisor/properties/features" or die "ERROR open(): $!";
 my $features = <FEAT>;
@@ -24,16 +25,18 @@ close FEAT;
 chomp $features;
 my $decfeatures = hex $features;
 
-my $VERSIONDIR= "/sys/hypervisor/version/";
-my @files = ("major", "minor", "extra");
+my $VERSIONDIR = "/sys/hypervisor/version/";
+my @files = ( "major", "minor", "extra" );
 my $version;
 foreach my $f (@files) {
-	foreach (cat_("$VERSIONDIR/$f")) {
-	chomp ($_);
-	# add missing . between major and minor
-	if ($f =~ /major/) { $_ = $_ . "." }
-	$version = $version . $_;
-}}
+    foreach ( cat_("$VERSIONDIR/$f") ) {
+        chomp($_);
+
+        # add missing . between major and minor
+        if ( $f =~ /major/ ) { $_ = $_ . "." }
+        $version = $version . $_;
+    }
+}
 
 my $capa = cat_("/sys/hypervisor/properties/capabilities");
 
@@ -42,16 +45,18 @@ print "Xen capabilities: $capa \n";
 print "Xen features: $features\n";
 
 foreach (<DATA>) {
-	my ($def, $feat, $bit) = split;
-	$feat =~ s/^XENFEAT_//;
-	if ($decfeatures & (1 << $bit)) {
-		print "+ ENABLED: $feat\n" 
-	} else {
-		print "-Disable: $feat\n" 
-	}
+    my ( $def, $feat, $bit ) = split;
+    $feat =~ s/^XENFEAT_//;
+    if ( $decfeatures & ( 1 << $bit ) ) {
+        print "+ ENABLED: $feat\n";
+    }
+    else {
+        print "-Disable: $feat\n";
+    }
 }
 
-print "\nhttp://xenbits.xen.org/docs/unstable-staging/hypercall/x86_64/include,public,features.h.html\n";
+print
+"\nhttp://xenbits.xen.org/docs/unstable-staging/hypercall/x86_64/include,public,features.h.html\n";
 
 # The following are from include/xen/interface/features.h, and will need updating:
 
